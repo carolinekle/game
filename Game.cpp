007bf08ca02b0a690b7348e1e6b4ce32f4bd913f
carolinekle <caroline.klewinowski@gmail.com>
2025-03-17 19:56:19 -0400

    get loop working

---
+++ Game.cpp
@@ -0,0 +1,40 @@
+#include "Game.h"
+#include <iostream>
+
+bool Game::Init()
+{
+    return true;
+}
+
+void Game::Input()
+{
+    std::cout << "input\n";
+}
+
+void Game::Update()
+{
+    std::cout << "update\n";
+}
+
+void Game::Draw()
+{
+    std::cout << "draw\n";
+}
+
+Game::Game() : isRunning(true) {}
+
+Game::~Game() {}
+
+void Game::Run()
+{
+    if (!Init())
+        isRunning = false;
+
+    while (isRunning)
+    {
+        Input();
+        Update();
+        Draw();
+        std::cout << "run\n";
+    }
+}
---
+++ Game.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+
+class Game
+{
+    private:
+    bool isRunning;
+    
+        bool Init();
+        void Input();
+        void Update();
+        void Draw();
+    public:
+        Game();
+        ~Game();
+    void Run();
+};
+
